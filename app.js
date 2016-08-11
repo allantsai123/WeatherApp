080f4667b423048a55216faf36bd542f17898fd8
allantsai123 <allantsai123@gmail.com>
2016-08-11 12:25:10 -0700

    update files
    finished the basic css and starting to do the js part

--- app.js
+++ app.js
@@ -1,3 +1,87 @@
+var APPID = "a261d0b5f91d49be3dc6b16ab109b71b";
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+function updateByCity(city,country){
+    var url = "http://api.openweathermap.org/data/2.5/weather?q="+ city+","+country+"&appid="+APPID;
+    sendRequest(url);
+}
+function sendRequest(url){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            var data = JSON.parse(xmlhttp.responseText);
+            var weather = {};
+            weather.icon = data.weather[0].id;
+            weather.humidity = data.main.humidity;
+            weather.wind = data.main.speed;
+            weather.direction = data.main.deg;
+            weather.loc = data.name;
+            weather.temp = data.main.temp;
+            update(weather);
+            console.log(data);
+        }
+    };
+    xmlhttp.open("GET",url,true);
+    xmlhttp.send();
+
+}
+
+
+function update(weather){
+    wind.innerHTML = weather.wind;
+    direction.innerHTML = weather.direction;
+    humidity.innerHTML = weather.humidity;
+    loc.innerHTML = weather.loc;
+    temp.innerHTML = weather.temp;
+    icon.src = "img/code/" + weather.icon + ".png";
+}
+
+window.onload = function(){
+    temp = document.getElementById("temperature");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    humidity = document.getElementById("humidity");
+    wind = document.getElementById("wind");
+    direction = document.getElementById("direction");
+
+    // var weather ={};
+    // weather.wind =3.5;
+    // weather.direction = "N";
+    // weather.humidity = 35;
+    // weather.loc = "vancouver";
+    // weather.temp = "145";
+    // weather.icon = 200;
+
+    // update(weather);
+
+    updateByCity("Vancouver,Ca");
+}
+
+
+
+
+
+function K2F(k){
+    return Math.round(k*(9/5)-459.67);
+}
+
+function K2C(k){
+    return Math.round(k - 273.15);
+}
+
+
+
+
+
+
+
+
+
 //current date and time for the brower's time zone
 (function () {
     function startTime() {
@@ -20,75 +104,3 @@
     }
     startTime();
 })();
-
-
-
-
-var APPID = "a261d0b5f91d49be3dc6b16ab109b71b";
-var temp;
-var loc;
-var icon;
-var humidity;
-var wind;
-var direction;
-
-
-
-
-
-window.onload = function () {
-    temp = document.getElementById("temperature");
-    loc = document.getElementById("location");
-    icon = document.getElementById("icon");
-    humidity = document.getElementById("humidity");
-    wind = document.getElementById("wind");
-    direction = document.getElementById("direction");
-
-    var weather={};
-    weather.wind=3.5;
-    weather.direction="N";
-    weather.humidity=35;
-    weather.loc="bos";
-    weather.temp='45';
-    weather.icon=200;
-
-    update(weather);
-}
-function update(weather) {
-    //icon.src = "imgs/codes/" + weather.code + ".png"
-
-    humidity.innerHTML = weather.humidity;
-    wind.innerHtml = weather.wind;
-    direction.innerHTML = weather.direction;
-    loc.innerHTML = weather.location;
-    temp.innerHTML = weather.temp;
-}
-
-function sendRequest(url){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var data = JSON.parse(xmlhttp.responseText);
-	    var weather = {};
-	    weather.code = data.weather[0].id;
-	    weather.humidity = data.main.humidity;
-	    weather.wind = data.wind.speed;
-	    /* NEW */
-	    weather.direction = degreesToDirection(data.wind.deg)
-	    weather.location = data.name;
-	    /* NEW */
-	    weather.temp = K2F(data.main.temp);		
-	    update(weather);
-	}
-    };
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();    
-}
-function C2F(k){
-    return Math.round(k*(9/5)-459.67);
-}
-
-function F2C(k){
-    return Math.round(k - 273.15);
-}
